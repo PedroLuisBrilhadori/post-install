44b80e147da4d218244a7ea96c511f460f0413d8
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2024-12-23 19:35:13 -0300

    chore: adiciona versão para wsl2

--- install-apps.sh
+++ install-apps.sh
@@ -10,15 +10,6 @@ cd yay
 # build and install yay
 makepkg -si
 
-
-## remove unsed apps 
-sudo pacman -Rns firefox
-
-
-## install window apps
-
-yay -S microsoft-edge-stable-bin  google-chrome visual-studio-code-bin discord steam mouse_m908 beekeeper-studio-bin insomnia 
-
 ## install cli apps
 
 yay -S exa bat github-cli vim docker docker-compose
@@ -29,9 +20,6 @@ yay -S exa bat github-cli vim docker docker-compose
 
 ## install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-## install nvm 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # reload zsh to path update 
 . ~/.zshrc
